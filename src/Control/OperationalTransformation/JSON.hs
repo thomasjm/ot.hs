83fae62c372e2ba29b07c5c2781f54c37308ae5f
Tom McLaughlin <pyro777@gmail.com>
2016-08-23 02:43:02 -0700

    JSON instances for JSONOperation

--- src/Control/OperationalTransformation/JSON.hs
+++ src/Control/OperationalTransformation/JSON.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE ScopedTypeVariables, OverloadedStrings #-}
 
 module Control.OperationalTransformation.JSON
   (
@@ -7,56 +7,12 @@ module Control.OperationalTransformation.JSON
   , invertOperation
   ) where
 
+
 import Control.OperationalTransformation
-import Data.Aeson
-import qualified Data.Aeson as A
-import Data.Text
-
-type Property = Text
-type Position = Int
-data PathSegment = Prop Property | Pos Position
-                 deriving Show
-type Path = [PathSegment]
-
-
-instance FromJSON PathSegment where
-  parseJSON (String a) = Prop <$> parseJSON (String a)
-  parseJSON (Number a) = Pos  <$> parseJSON (Number a)
-  parseJSON _ = fail "Invalid Pathsegment"
-
+import Control.OperationalTransformation.JSON.Types
 
 invertOperation = undefined
 
--- https://github.com/josephg/ShareJS/blob/master/lib/types/json-api.js
-
--- {a: [1, 0, {b: {}}]}
-
--- insert(["a", 0], 'qasdlkfasdfkljasdflkja') -> {a: ['q', 1, 0, {b: {}}]}
-
--- set(["a", 2, "bkasdflkasdlkfjasdflkjasdflkj"], {})
-
-newtype Length = Length Int
-newtype Index = Index Int
-
-
-data JSONOperation =
-  Get Path
-  | Set Path
-  | Remove Path Int
-  | Insert Path A.Value
-  | Move Path Path Path -- ???
-  | Add Path Int
-  | DeleteText Path Length Index
-
-
+-- TODO: implement these operations
 instance OTOperation JSONOperation where
-  transform (Set path1) (Set path2) = undefined
-
-
--- Misc:
--- get(path)
--- getLength(path) -- just does this.get(path).length
--- hmm, why isn't there insertText?
---
---
---
+  transform (ListInsert path1 index1 value1) (ListInsert path2 index2 value2) = undefined
---
+++ src/Control/OperationalTransformation/JSON/Types.hs
@@ -0,0 +1,125 @@
+{-# LANGUAGE ScopedTypeVariables, OverloadedStrings #-}
+
+module Control.OperationalTransformation.JSON.Types where
+
+import Control.OperationalTransformation.Text (TextOperation)
+import Data.Aeson
+import qualified Data.Aeson as A
+import qualified Data.Aeson.Types as A
+import qualified Data.HashMap.Strict as HM
+import qualified Data.Text as T
+
+-- * PathSegment and Path
+
+data PathSegment = Prop T.Text | Pos Int deriving Show
+type Path = [PathSegment]
+
+instance FromJSON PathSegment where
+  parseJSON (String a) = Prop <$> parseJSON (String a)
+  parseJSON (Number a) = Pos  <$> parseJSON (Number a)
+  parseJSON _ = fail "Invalid Pathsegment"
+
+-- * JSONOperation
+
+-- Based on the "Summary of operations" at https://github.com/ottypes/json0
+-- See also https://github.com/josephg/ShareJS/blob/master/lib/types/json-api.js
+data JSONOperation =
+  -- * Numbers
+
+  -- adds the number to the number at [path]
+  Add Path Int
+
+  -- * Lists
+
+  -- inserts the object obj before the item at idx in the list at [path]
+  | ListInsert Path Int A.Value
+  -- deletes the object obj from the index idx in the list at [path]
+  | ListDelete Path Int A.Value
+  -- replaces the object before at the index in the list at [path] with the object after
+  | ListReplace Path Int A.Value A.Value
+  -- moves the object at idx1 such that the object will be at index idx2 in the list at [path]
+  | ListMove Path Int Int
+
+  -- * Objects
+
+  -- inserts the object obj into the object at [path] with key key
+  | ObjectInsert Path T.Text A.Value
+  -- deletes the object obj with key key from the object at [path]
+  | ObjectDelete Path T.Text A.Value
+  -- replaces the object before with the object after at key key in the object at [path]
+  | ObjectReplace Path T.Text A.Value A.Value
+
+  -- * Subtypes
+
+  -- applies the subtype op o of type t to the object at [path]
+  | ApplySubtypeOperation Path TextOperation
+  -- inserts the string s at offset offset into the string at [path] (uses subtypes internally)
+  | InsertString Path Int T.Text
+  -- deletes the string s at offset offset from the string at [path] (uses subtypes internally)
+  | DeleteString Path Int T.Text
+
+
+instance FromJSON JSONOperation where
+  parseJSON (A.Object v) | "na" `elem` (HM.elems v) = Add <$> v .: "p" <*> v .: "na"
+
+  -- Lists
+  parseJSON (A.Object v) | "li" `elem` (HM.elems v) = do
+                             (path, index) <- parsePathAndIndex v
+                             obj <- v .: "li"
+                             return $ ListInsert path index obj
+  parseJSON (A.Object v) | "ld" `elem` (HM.elems v) && "li" `elem` (HM.elems v) = do
+                             (path, index) <- parsePathAndIndex v
+                             before <- v .: "ld"
+                             after <- v .: "li"
+                             return $ ListReplace path index before after
+  parseJSON (A.Object v) | "ld" `elem` (HM.elems v) = do
+                             (path, index) <- parsePathAndIndex v
+                             obj <- v .: "ld"
+                             return $ ListDelete path index obj
+  parseJSON (A.Object v) | "lm" `elem` (HM.elems v) = do
+                             (path, index1) <- parsePathAndIndex v
+                             index2 <- v .: "lm"
+                             return $ ListMove path index1 index2
+
+  -- Objects
+  parseJSON (A.Object v) | "oi" `elem` (HM.elems v) = do
+                             (path, prop) <- parsePathAndProp v
+                             obj <- v .: "oi"
+                             return $ ObjectInsert path prop obj
+  parseJSON (A.Object v) | "od" `elem` (HM.elems v) && "oi" `elem` (HM.elems v) = do
+                             (path, prop) <- parsePathAndProp v
+                             before <- v .: "od"
+                             after <- v .: "oi"
+                             return $ ObjectReplace path prop before after
+  parseJSON (A.Object v) | "od" `elem` (HM.elems v) = do
+                             (path, prop) <- parsePathAndProp v
+                             obj <- v .: "od"
+                             return $ ObjectDelete path prop obj
+
+  -- Subtypes
+  parseJSON (A.Object v) | "o" `elem` (HM.elems v) = ApplySubtypeOperation <$> v .: "p" <*> v .: "o"
+  parseJSON (A.Object v) | "si" `elem` (HM.elems v) = do
+                             (path, index) <- parsePathAndIndex v
+                             str <- v .: "si"
+                             return $ InsertString path index str
+  parseJSON (A.Object v) | "sd" `elem` (HM.elems v) = do
+                             (path, index) <- parsePathAndIndex v
+                             str <- v .: "sd"
+                             return $ DeleteString path index str
+  parseJSON _ = fail "Failed to parse operation"
+
+
+
+parsePathAndIndex :: A.Object -> A.Parser ([PathSegment], Int)
+parsePathAndIndex v = do
+  pathAndIndex :: [PathSegment]  <- v .: "p"
+  let path = init pathAndIndex
+  let (Pos index) = last pathAndIndex
+  return (path, index)
+
+parsePathAndProp :: A.Object -> A.Parser ([PathSegment], T.Text)
+parsePathAndProp v = do
+  pathAndProp :: [PathSegment]  <- v .: "p"
+  let path = init pathAndProp
+  let (Prop prop) = last pathAndProp
+  return (path, prop)
