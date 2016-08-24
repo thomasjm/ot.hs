46165aa915a88f1f504cc7f969beb5b8ae637720
Tom McLaughlin <pyro777@gmail.com>
2016-08-23 21:07:56 -0700

    Run JSON tests

--- test/Main.hs
+++ test/Main.hs
@@ -1,15 +1,19 @@
 module Main (main) where
 
-import Test.Tasty
-
 import qualified Control.OperationalTransformation.ClientServerTests
+import qualified Control.OperationalTransformation.JSON.Specs
 import qualified Control.OperationalTransformation.JSON.Tests
 import qualified Control.OperationalTransformation.Selection.Tests
 import qualified Control.OperationalTransformation.Text.Tests
+import Test.Tasty
+import Test.Tasty.Hspec
 
 main :: IO ()
-main = defaultMain $ testGroup "All tests" [Control.OperationalTransformation.ClientServerTests.tests
-                                           , Control.OperationalTransformation.JSON.Tests.tests
-                                           , Control.OperationalTransformation.Selection.Tests.tests
-                                           , Control.OperationalTransformation.Text.Tests.tests
-                                           ]
+main = do
+  jsonHspecTests <- testSpec "JSON specs" Control.OperationalTransformation.JSON.Specs.specs
+  defaultMain $ testGroup "All tests" [Control.OperationalTransformation.ClientServerTests.tests
+                                      , jsonHspecTests
+                                      , Control.OperationalTransformation.JSON.Tests.tests
+                                      , Control.OperationalTransformation.Selection.Tests.tests
+                                      , Control.OperationalTransformation.Text.Tests.tests
+                                      ]
